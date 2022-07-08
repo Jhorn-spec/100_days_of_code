2fcab46a2441e6501a397fc8e0354e954bd0ec32
Ajayi John <biodunajayijohn@gmail.com>
2022-07-08 21:56:40 +0100

    start day 11 project

--- day_11-blackjack/main.py
+++ day_11-blackjack/main.py
@@ -50,6 +50,23 @@ def calculate_score(cards):
     return sum(cards)
 
 
+def compare(user_score, computer_score):
+    if user_score == computer_score:
+        return 'Draw'
+    elif computer_score == 0:
+        return "Lose, opponent has Blackjack"
+    elif user_score == 0:
+        return "Win with a Blackjack"
+    elif user_score > 21:
+        return "You went over. You lose"
+    elif computer_score > 21:
+        return "Opponent went over. You win"
+    elif user_score > computer_score:
+        return "You win"
+    else:
+        return "You lose"
+
+
 # Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
 user_cards = []
 computer_cards = []
@@ -59,21 +76,26 @@ for _ in range(0, 2):
     user_cards.append(deal_card())
     computer_cards.append(deal_card())
 
-user_score = calculate_score(user_cards)
-computer_score = calculate_score(computer_cards)
-print(f" Your cards: {user_cards}, current score: {user_score}")
-print(f" Computer's first card: {computer_cards[0]}")
+while not is_game_over:
+    user_score = calculate_score(user_cards)
+    computer_score = calculate_score(computer_cards)
+    print(f' Your cards: {user_cards}, current score: {user_score}')
+    print(f' Computer\'s first card: {computer_cards[0]}')
 
-if user_score == 0 or computer_score == 0 or user_score > 0:
-    is_game_over = True
-else:
-    user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")
-    if user_should_deal == "y":
-        user_cards.append(deal_card())
-    else:
+    if user_score == 0 or computer_score == 0 or user_score > 0:
         is_game_over = True
+    else:
+        user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")
+        if user_should_deal == "y":
+            user_cards.append(deal_card())
+        else:
+            is_game_over = True
 
+while computer_score != 0 and computer_score < 17:
+    computer_cards.append(deal_card())
+    computer_score = calculate_score(computer_cards)
 
+    print(compare(user_score, computer_score))
 
 
 # Hint 10: If the game has not ended, ask the user if they want to draw another card.
